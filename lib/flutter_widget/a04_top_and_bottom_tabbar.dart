f9b085ff7e0d1af8a66ebda2561f4e6c7542d68c
ywarahi <y.warahi@gmail.com>
2021-12-19 17:19:06 +0900

    update top_and_bottom_tabbar, may be OK.

--- lib/flutter_widget/a04_top_and_bottom_tabbar.dart
+++ lib/flutter_widget/a04_top_and_bottom_tabbar.dart
@@ -1,70 +1,117 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  static const String _title = 'TabBar x BottomBar';
+  static const String _title = 'TopTabBar x BottomTabBar';
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: _title, home: MyTabbedPage());
+    return MaterialApp(title: _title, home: MyTabbedPage());
   }
 }
 
 class MyTabbedPage extends StatefulWidget {
-  const MyTabbedPage({Key? key}) : super(key: key);
+  MyTabbedPage({Key? key}) : super(key: key);
+
+  final _topTabList = <List<Tab>>[
+    const <Tab>[
+      Tab(icon: Icon(Icons.cloud_outlined), text: 'Home1'),
+      Tab(icon: Icon(Icons.beach_access_sharp), text: 'Home2'),
+      Tab(icon: Icon(Icons.brightness_5_sharp), text: 'Home3'),
+    ],
+    const <Tab>[
+      Tab(icon: Icon(Icons.cloud_outlined), text: 'Search1'),
+      Tab(icon: Icon(Icons.beach_access_sharp), text: 'Search2'),
+      Tab(icon: Icon(Icons.brightness_5_sharp), text: 'Search3'),
+    ],
+    const <Tab>[
+      Tab(icon: Icon(Icons.cloud_outlined), text: 'About1'),
+      Tab(icon: Icon(Icons.beach_access_sharp), text: 'About2'),
+      Tab(icon: Icon(Icons.brightness_5_sharp), text: 'About3'),
+      Tab(icon: Icon(Icons.access_time_filled_sharp), text: 'About4'),
+    ]
+  ];
+
+  final List<BottomNavigationBarItem> _bottomTabs = [
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'Home',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.search),
+      label: 'Search',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.search),
+      label: 'About',
+    ),
+  ];
 
   @override
   State<MyTabbedPage> createState() => _MyTabbedPageState();
 }
 
 class _MyTabbedPageState extends State<MyTabbedPage>
-    with SingleTickerProviderStateMixin {
-  static const List<Tab> myTabs = <Tab>[
-    Tab(text: 'LEFT'),
-    Tab(text: 'RIGHT'),
-  ];
+    with TickerProviderStateMixin {
 
-  final int _bottomTabIndex = 0;
-  final int _bottomTabLength = 1;
-  List<TabController> _tabController = [];
+  TabController? _tabController;
+  int _bottomTabIndex = 0;
+  final List<int> _topTabIndexList = [];
 
   @override
   void initState() {
     super.initState();
-    for (var i = 0; i < _bottomTabLength; i++) {
-      _tabController.add(TabController(vsync: this, length: myTabs.length));
+    //_tabController = TabController(vsync: this, length: myTabs.length);
+    for (var i = 0; i < widget._bottomTabs.length; i++) {
+      _topTabIndexList.add(0);
     }
   }
 
   @override
   void dispose() {
-    for (var i = 0; i < _bottomTabLength; i++) {
-      _tabController[i].dispose();
-    }
+    _tabController?.dispose();
     super.dispose();
+  }
+
+  void onBottomBarTap(int index) {
+    _topTabIndexList[_bottomTabIndex] = _tabController!.index;
+    _bottomTabIndex = index;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
+    print('build is called.');
+
+    _tabController?.dispose();
+    _tabController = TabController(
+        vsync: this,
+        length: widget._topTabList[_bottomTabIndex].length,
+        initialIndex: _topTabIndexList[_bottomTabIndex]);
+
     return Scaffold(
-      appBar: AppBar(
-        bottom: TabBar(
-          controller: _tabController[_bottomTabIndex],
-          tabs: myTabs,
+        appBar: AppBar(
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: widget._topTabList[_bottomTabIndex],
+          ),
         ),
-      ),
-      body: TabBarView(
-        controller: _tabController[_bottomTabIndex],
-        children: myTabs.map((Tab tab) {
-          final label = tab.text!.toLowerCase();
-          return Center(
-            child: Text(
-              'This is the $_bottomTabIndex / $label tab',
-              style: const TextStyle(fontSize: 36),
-            ),
-          );
-        }).toList(),
-      ),
-    );
+        body: TabBarView(
+          controller: _tabController,
+          children: widget._topTabList[_bottomTabIndex].map((Tab tab) {
+            final label = tab.text!.toLowerCase();
+            return Center(
+              child: Text(
+                'This is the $_bottomTabIndex / $label tab',
+                style: const TextStyle(fontSize: 36),
+              ),
+            );
+          }).toList(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: widget._bottomTabs,
+          currentIndex: _bottomTabIndex,
+          onTap: onBottomBarTap,
+        ));
   }
 }
 
