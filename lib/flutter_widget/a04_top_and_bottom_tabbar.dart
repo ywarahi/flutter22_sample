4f13f4a19fab79800caabcb3f798f2d322bd5ad3
ywarahi <y.warahi@gmail.com>
2021-12-25 18:25:02 +0900

    update top_and_bottom_tabar, may be OK.

--- lib/flutter_widget/a04_top_and_bottom_tabbar.dart
+++ lib/flutter_widget/a04_top_and_bottom_tabbar.dart
@@ -1,50 +1,89 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  static const String _title = 'TopTabBar x BottomTabBar';
+final bottomTabs = <BottomNavigationBarItem>[
+  const BottomNavigationBarItem(
+    icon: Icon(Icons.home),
+    label: 'Home',
+  ),
+  const BottomNavigationBarItem(
+    icon: Icon(Icons.category),
+    label: 'Category',
+  ),
+  const BottomNavigationBarItem(
+    icon: Icon(Icons.search),
+    label: 'Search',
+  ),
+  const BottomNavigationBarItem(
+    icon: Icon(Icons.search),
+    label: 'About',
+  ),
+];
 
+final topTabsList = <List<Tab>>[
+  const <Tab>[
+    Tab(icon: Icon(Icons.cloud_outlined), text: 'Home1'),
+    // Tab(icon: Icon(Icons.beach_access_sharp), text: 'Home2'),
+    // Tab(icon: Icon(Icons.brightness_5_sharp), text: 'Home3'),
+  ],
+  const <Tab>[
+    Tab(icon: Icon(Icons.cloud_outlined), text: 'Category1'),
+    Tab(icon: Icon(Icons.beach_access_sharp), text: 'Category2'),
+    Tab(icon: Icon(Icons.brightness_5_sharp), text: 'Category3'),
+  ],
+  const <Tab>[
+    Tab(icon: Icon(Icons.cloud_outlined), text: 'Search1'),
+    Tab(icon: Icon(Icons.beach_access_sharp), text: 'Search2'),
+    Tab(icon: Icon(Icons.brightness_5_sharp), text: 'Search3'),
+  ],
+  const <Tab>[
+    Tab(icon: Icon(Icons.cloud_outlined), text: 'About1'),
+    Tab(icon: Icon(Icons.beach_access_sharp), text: 'About2'),
+    Tab(icon: Icon(Icons.brightness_5_sharp), text: 'About3'),
+    Tab(icon: Icon(Icons.access_time_filled_sharp), text: 'About4'),
+  ]
+];
+
+final pagesList = <List<Widget>>[
+  const <Widget>[
+    CustomPage(panelColor: Colors.cyan, title: 'Home1'),
+    // CustomPage(panelColor: Colors.pink, title: 'Home2'),
+    // CustomPage(panelColor: Colors.amberAccent, title: 'Home3'),
+  ],
+  const <Widget>[
+    CustomPage(panelColor: Colors.cyan, title: 'Category1'),
+    CustomPage(panelColor: Colors.pink, title: 'Category2'),
+    CustomPage(panelColor: Colors.amberAccent, title: 'Category3'),
+  ],
+  const <Widget>[
+    CustomPage(panelColor: Colors.cyan, title: 'Search1'),
+    CustomPage(panelColor: Colors.pink, title: 'Search2'),
+    CustomPage(panelColor: Colors.amberAccent, title: 'Search3'),
+  ],
+  const <Widget>[
+    CustomPage(panelColor: Colors.cyan, title: 'About1'),
+    CustomPage(panelColor: Colors.pink, title: 'About2'),
+    CustomPage(panelColor: Colors.amberAccent, title: 'About3'),
+    CustomPage(panelColor: Colors.amberAccent, title: 'About4'),
+  ],
+];
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: _title, home: MyTabbedPage());
+    return MaterialApp(
+        title: 'TopTabBar x BottomTabBar',
+        home: MyTabbedPage(bottomTabs, topTabsList, pagesList));
   }
 }
 
 class MyTabbedPage extends StatefulWidget {
-  MyTabbedPage({Key? key}) : super(key: key);
+  const MyTabbedPage(this._bottomTabs, this._topTabList, this._pagesList,
+      {Key? key})
+      : super(key: key);
 
-  final _topTabList = <List<Tab>>[
-    const <Tab>[
-      Tab(icon: Icon(Icons.cloud_outlined), text: 'Home1'),
-      Tab(icon: Icon(Icons.beach_access_sharp), text: 'Home2'),
-      Tab(icon: Icon(Icons.brightness_5_sharp), text: 'Home3'),
-    ],
-    const <Tab>[
-      Tab(icon: Icon(Icons.cloud_outlined), text: 'Search1'),
-      Tab(icon: Icon(Icons.beach_access_sharp), text: 'Search2'),
-      Tab(icon: Icon(Icons.brightness_5_sharp), text: 'Search3'),
-    ],
-    const <Tab>[
-      Tab(icon: Icon(Icons.cloud_outlined), text: 'About1'),
-      Tab(icon: Icon(Icons.beach_access_sharp), text: 'About2'),
-      Tab(icon: Icon(Icons.brightness_5_sharp), text: 'About3'),
-      Tab(icon: Icon(Icons.access_time_filled_sharp), text: 'About4'),
-    ]
-  ];
-
-  final List<BottomNavigationBarItem> _bottomTabs = [
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      label: 'Home',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.search),
-      label: 'Search',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.search),
-      label: 'About',
-    ),
-  ];
+  final List<List<Tab>> _topTabList;
+  final List<BottomNavigationBarItem> _bottomTabs;
+  final List<List<Widget>> _pagesList;
 
   @override
   State<MyTabbedPage> createState() => _MyTabbedPageState();
@@ -52,7 +91,6 @@ class MyTabbedPage extends StatefulWidget {
 
 class _MyTabbedPageState extends State<MyTabbedPage>
     with TickerProviderStateMixin {
-
   TabController? _tabController;
   int _bottomTabIndex = 0;
   final List<int> _topTabIndexList = [];
@@ -72,7 +110,40 @@ class _MyTabbedPageState extends State<MyTabbedPage>
     super.dispose();
   }
 
-  void onBottomBarTap(int index) {
+  TabBar? getTopTabBar() {
+    if (widget._pagesList[_bottomTabIndex].length == 1) {
+      return null;
+    } else {
+      return TabBar(
+        controller: _tabController,
+        tabs: widget._topTabList[_bottomTabIndex],
+      );
+    }
+  }
+
+  Widget getCurrentPage() {
+    if (widget._pagesList[_bottomTabIndex].length == 1) {
+      return widget._pagesList[_bottomTabIndex][0];
+    } else {
+      // int topTabIndex = (_tabController?.index)??
+      //     _topTabIndexList[_bottomTabIndex]?? 0;
+      return TabBarView(
+        controller: _tabController,
+        children: widget._pagesList[_bottomTabIndex],
+      );
+    }
+  }
+
+  BottomNavigationBar getBottomTabBar() {
+    return BottomNavigationBar(
+      items: widget._bottomTabs,
+      currentIndex: _bottomTabIndex,
+      onTap: onTapBottomBar,
+      type: BottomNavigationBarType.fixed,
+    );
+  }
+
+  void onTapBottomBar(int index) {
     _topTabIndexList[_bottomTabIndex] = _tabController!.index;
     _bottomTabIndex = index;
     setState(() {});
@@ -90,28 +161,42 @@ class _MyTabbedPageState extends State<MyTabbedPage>
 
     return Scaffold(
         appBar: AppBar(
-          bottom: TabBar(
-            controller: _tabController,
-            tabs: widget._topTabList[_bottomTabIndex],
+          bottom: getTopTabBar(),
+        ),
+        body: getCurrentPage(),
+        bottomNavigationBar: getBottomTabBar());
+  }
+}
+
+class CustomPage extends StatelessWidget {
+  const CustomPage({required this.panelColor, required this.title});
+
+  final Color panelColor;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final titleTextStyle = Theme.of(context).textTheme.headline6;
+    return Container(
+      child: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+              color: panelColor,
+              borderRadius: const BorderRadius.all(Radius.circular(20))),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: titleTextStyle!.fontSize,
+                color: titleTextStyle.color,
+              ),
+            ),
           ),
         ),
-        body: TabBarView(
-          controller: _tabController,
-          children: widget._topTabList[_bottomTabIndex].map((Tab tab) {
-            final label = tab.text!.toLowerCase();
-            return Center(
-              child: Text(
-                'This is the $_bottomTabIndex / $label tab',
-                style: const TextStyle(fontSize: 36),
-              ),
-            );
-          }).toList(),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: widget._bottomTabs,
-          currentIndex: _bottomTabIndex,
-          onTap: onBottomBarTap,
-        ));
+      ),
+    );
   }
 }
 
