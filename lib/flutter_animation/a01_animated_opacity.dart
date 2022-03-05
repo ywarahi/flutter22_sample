fc40376af634fc0bf81b59b9ec3ea1c4eddebafe
ywarahi <y.warahi@gmail.com>
2022-03-05 16:00:49 +0900

    Update a01_animated_opacity.dart

--- lib/flutter_animation/a01_animated_opacity.dart
+++ lib/flutter_animation/a01_animated_opacity.dart
@@ -3,80 +3,84 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyAnmatedPage(),
-    );
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Widget'),
+      ),
+      body: const MyAnimatedPage(),
+    ));
   }
 }
 
-class MyAnmatedPage extends StatefulWidget {
-  const MyAnmatedPage({Key? key}) : super(key: key);
+class MyAnimatedPage extends StatefulWidget {
+  const MyAnimatedPage({Key? key}) : super(key: key);
 
   @override
-  _MyAnmatedPageState createState() => _MyAnmatedPageState();
+  _MyAnimatedPageState createState() => _MyAnimatedPageState();
 }
 
-class _MyAnmatedPageState extends State<MyAnmatedPage> with TickerProviderStateMixin {
+class _MyAnimatedPageState extends State<MyAnimatedPage>
+    with TickerProviderStateMixin {
   bool flag = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Animated Widget'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AnimatedOpacity(
+              opacity: flag ? 0.0 : 1.0,
+              duration: const Duration(seconds: 3),
+              child: Text(
+                '消える文字(0 ⇨ 1)',
+                style: Theme.of(context).textTheme.headline4,
+              )),
+          AnimatedOpacity(
+              opacity: flag ? 1.0 : 0.0,
+              duration: const Duration(seconds: 3),
+              child: Text(
+                '消える文字(1 ⇨ 0)',
+                style: Theme.of(context).textTheme.headline4,
+              )),
+          buildControlButtons(),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            AnimatedOpacity(
-                opacity: flag ? 0.0 : 1.0,
-                duration: const Duration(seconds: 3),
-                child: Text(
-                  '消える文字(0 ⇨ 1)',
-                  style: Theme.of(context).textTheme.headline4,
-                )),
-            AnimatedOpacity(
-                opacity: flag ? 1.0 : 0.0,
-                duration: const Duration(seconds: 3),
-                child: Text(
-                  '消える文字(1 ⇨ 0)',
-                  style: Theme.of(context).textTheme.headline4,
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      flag = !flag;
-                    });
-                  },
-                  icon: const Icon(Icons.arrow_back),
-                  color: Colors.blue,
-                  iconSize: 32,
-                ),
-                const IconButton(
-                  onPressed: null,
-                  icon: Icon(Icons.stop),
-                  color: Colors.pink,
-                  iconSize: 32,
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      flag = !flag;
-                    });
-                  },
-                  icon: const Icon(Icons.arrow_forward),
-                  color: Colors.blue,
-                  iconSize: 32,
-                ),
-              ],
-            ),
-          ],
+    );
+  }
+
+  Widget buildControlButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: () {
+            setState(() {
+              flag = !flag;
+            });
+          },
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.blue,
+          iconSize: 32,
         ),
-      ),
+        const IconButton(
+          onPressed: null,
+          icon: Icon(Icons.stop),
+          color: Colors.pink,
+          iconSize: 32,
+        ),
+        IconButton(
+          onPressed: () {
+            setState(() {
+              flag = !flag;
+            });
+          },
+          icon: const Icon(Icons.arrow_forward),
+          color: Colors.blue,
+          iconSize: 32,
+        ),
+      ],
     );
   }
 }
