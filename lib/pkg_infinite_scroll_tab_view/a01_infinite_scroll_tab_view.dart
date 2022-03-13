79d6a5e4cd2e73457b7361301c23037022effead
ywarahi <y.warahi@gmail.com>
2022-03-13 22:49:32 +0900

    add pkg_infinite_scroll_tab_view.

--- lib/pkg_infinite_scroll_tab_view/a01_infinite_scroll_tab_view.dart
+++ lib/pkg_infinite_scroll_tab_view/a01_infinite_scroll_tab_view.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_tab_view/infinite_scroll_tab_view.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+final topTabItems = <Text>[
+  const Text('1'),
+  const Text('22'),
+  const Text('333'),
+  const Text('444'),
+  const Text('55'),
+  const Text('6'),
+];
+
+final topTabPages = <Widget>[
+  const CustomPage(panelColor: Colors.cyan, title: '1'),
+  const CustomPage(panelColor: Colors.green, title: '22'),
+  const CustomPage(panelColor: Colors.pink, title: '333'),
+  const CustomPage(panelColor: Colors.cyan, title: '444'),
+  const CustomPage(panelColor: Colors.green, title: '55'),
+  const CustomPage(panelColor: Colors.pink, title: '6')
+];
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,21 +29,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('InfiniteScrollTabView Demo'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('InfiniteScrollTabView Demo'),
+        ),
+        body: const _Content(),
       ),
-      body: const _Content(),
     );
   }
 }
@@ -42,48 +47,53 @@ class _Content extends StatefulWidget {
 }
 
 class __ContentState extends State<_Content> {
-  final contents = List.generate(9, (index) => index + 1)..shuffle();
-
-  String _convertContent(int number) =>
-      List.generate(number, (_) => '$number').join('');
-
   @override
   Widget build(BuildContext context) {
     return InfiniteScrollTabView(
-      contentLength: contents.length,
+      contentLength: topTabItems.length,
       onTabTap: (index) {
         debugPrint('tapped $index');
       },
-      tabBuilder: (index, isSelected) => Text(
-        _convertContent(contents[index]),
-        style: TextStyle(
-          color: isSelected ? Colors.pink : Colors.black54,
-          fontWeight: FontWeight.bold,
-          fontSize: 18,
-        ),
-      ),
+      tabBuilder: (index, isSelected) => topTabItems[index],
       separator: const BorderSide(color: Colors.black12, width: 2.0),
       onPageChanged: (index) => debugPrint('page changed to $index.'),
       indicatorColor: Colors.pink,
       pageBuilder: (context, index, _) {
-        return SizedBox.expand(
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: Colors.green.withOpacity(contents[index] / 10),
-            ),
-            child: Center(
-              child: Text(
-                _convertContent(contents[index]),
-                style: Theme.of(context).textTheme.headline3!.copyWith(
-                  color: contents[index] / 10 > 0.6
-                      ? Colors.white
-                      : Colors.black87,
-                ),
-              ),
-            ),
-          ),
-        );
+        return topTabPages[index];
       },
     );
   }
 }
+
+class CustomPage extends StatelessWidget {
+  const CustomPage({required this.panelColor, required this.title});
+
+  final Color panelColor;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final titleTextStyle = Theme.of(context).textTheme.headline6;
+    return Container(
+      child: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+              color: panelColor,
+              borderRadius: const BorderRadius.all(Radius.circular(20))),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: titleTextStyle!.fontSize,
+                color: titleTextStyle.color,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
