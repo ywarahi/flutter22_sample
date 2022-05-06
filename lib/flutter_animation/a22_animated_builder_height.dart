a7346fa76048a31cbf10110a99da5924f765e9b4
ywarahi <y.warahi@gmail.com>
2022-05-06 13:48:27 +0900

    add animation height-size-change.

---
+++ lib/flutter_animation/a22_animated_builder_height.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Animated Widget'),
+          ),
+          body: const MyAnimatedPage(),
+        ));
+  }
+}
+
+class MyAnimatedPage extends StatefulWidget {
+  const MyAnimatedPage({Key? key}) : super(key: key);
+
+  @override
+  _MyAnimatedPageState createState() => _MyAnimatedPageState();
+}
+
+// ①StateでAnimationを生成 & TickerProviderStateMixinを適用
+class _MyAnimatedPageState extends State<MyAnimatedPage>
+    with TickerProviderStateMixin {
+  // ②AnimationControllerを定義
+  late AnimationController _animationController;
+
+  late Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // ③initStateでAnimationControllerを初期化（thisを使用しているため）
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _animation = CurveTween(
+      curve: Curves.easeInCubic, //.bounceIn, //.easeInOutSine,
+    ).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    // ④disposeでAnimationControllerを破棄
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildControlButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: () {
+            setState(() {
+              _animationController.reverse();
+            });
+          },
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.blue,
+          iconSize: 32,
+        ),
+        IconButton(
+          onPressed: () {
+            setState(() {
+              _animationController.stop();
+            });
+          },
+          icon: const Icon(Icons.stop),
+          color: Colors.pink,
+          iconSize: 32,
+        ),
+        IconButton(
+          onPressed: () {
+            setState(() {
+              _animationController.forward();
+            });
+          },
+          icon: const Icon(Icons.arrow_forward),
+          color: Colors.blue,
+          iconSize: 32,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: _buildControlButtons(),
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: buildAnimatedWidget(),
+        ),
+      ],
+    );
+  }
+
+  Widget buildAnimatedWidget() {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, _) {
+        return SizedBox(
+            width: 100,
+            height: 100 * (_animation.value as double),
+            child: Container(color: Colors.green));
+      },
+    );
+  }
+}
