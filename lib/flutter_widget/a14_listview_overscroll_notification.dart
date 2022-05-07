8c8c438e0fcbced4d21f2499fbc08795cdef90af
ywarahi <y.warahi@gmail.com>
2022-05-07 17:26:25 +0900

    Update a14_listview_overscroll_notification.dart

--- lib/flutter_widget/a14_listview_overscroll_notification.dart
+++ lib/flutter_widget/a14_listview_overscroll_notification.dart
@@ -20,53 +20,174 @@ class MyListViewPage extends StatefulWidget {
   }
 }
 
-class _MyListViewPageState extends State {
+class _MyListViewPageState extends State with TickerProviderStateMixin {
   final _items = <String>[];
-  var _lock = false;
+
+  bool _loadingStatus = false;
+  AnimationStatus _popupStatus = AnimationStatus.dismissed;
+  bool _isActiveUpperSide = false;
+
+  late AnimationController _animationController;
+  late Animation _animation;
 
   @override
   void initState() {
     super.initState();
-    initData();
+
+    _onInit();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _animation = CurveTween(
+      curve: Curves.easeInCubic,
+    ).animate(_animationController);
+
+    _animationController.addStatusListener((status) {
+      _popupStatus = status;
+      if (status == AnimationStatus.dismissed) {
+        //stopped at the beginning
+        // print('dismissed');
+      } else if (status == AnimationStatus.forward) {
+        // print('forward');
+      } else if (status == AnimationStatus.completed) {
+        //stopped at the end
+        print('completed');
+        Future<void>.delayed(const Duration(seconds: 1)).then((e) {
+          setState(() {
+            print('popdown');
+            _animationController.reverse();
+          });
+        });
+      } else if (status == AnimationStatus.reverse) {
+        // print('reverse');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return NotificationListener<ScrollNotification>(
-        child: ListView.separated(
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) {
-              return ListTile(title: Text('${_items[index]}'));
-            },
-            separatorBuilder: (context, index) {
-              return const Divider(
-                color: Colors.blue,
-              );
-            },
-            itemCount: _items.length),
-        onNotification: (notification) {
-          final metrics = notification.metrics;
+    final deviceWidth = MediaQuery.of(context).size.width;
 
-          //print('${metrics.extentBefore} /  ${metrics.extentAfter}');
+    return Stack(
+      children: [
+        NotificationListener<ScrollNotification>(
+            child: ListView.separated(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text('${_items[index]}'));
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider(
+                    color: Colors.blue,
+                  );
+                },
+                itemCount: _items.length),
+            onNotification: (notification) {
+              final metrics = notification.metrics;
+              final max = notification.metrics.maxScrollExtent;
 
-          if (metrics.extentBefore == 0.0 &&
-              metrics.extentAfter > 600.0 &&
-              !_lock) {
-            _lock = true;
-            _onRefresh();
-          }
-          if (metrics.extentBefore > 600.0 &&
-              metrics.extentAfter == 0.0 &&
-              !_lock) {
-            _lock = true;
-            _onLoad();
-          }
+              //print('${metrics.extentBefore} /  ${metrics.extentAfter}');
+              //print(notification.toString());
 
-          return false;
-        });
+              if ( //metrics.extentBefore == 0.0 &&
+                  //metrics.extentAfter > 600.0 &&
+                  notification is ScrollEndNotification &&
+                      metrics.extentAfter == max &&
+                      _popupStatus == AnimationStatus.dismissed) {
+                _isActiveUpperSide = true;
+                setState(() {
+                  _animationController.forward();
+                });
+                _onRefresh();
+              }
+              if ( //metrics.extentBefore > 600.0 &&
+                  //metrics.extentAfter == 0.0 &&
+                  notification is ScrollEndNotification &&
+                      metrics.extentBefore == max &&
+                      _popupStatus == AnimationStatus.dismissed) {
+                _isActiveUpperSide = false;
+                setState(() {
+                  _animationController.forward();
+                });
+              }
+
+              return false;
+            }),
+        Visibility(
+          visible: _isActiveUpperSide && !_loadingStatus,
+          child: AnimatedBuilder(
+              animation: _animation,
+              builder: (context, _) {
+                return Positioned(
+                  left: 0,
+                  top: 0,
+                  width: deviceWidth,
+                  height: 100 * (_animation.value as double),
+                  child: Align(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _loadingStatus = true;
+                          _animationController.reset();
+                          _onRefresh();
+                        });
+                      },
+                      child: const Text('最新記事を表示'),
+                    ),
+                  ),
+                );
+              }),
+        ),
+        Visibility(
+          visible: !_isActiveUpperSide  && !_loadingStatus,
+          child: AnimatedBuilder(
+              animation: _animation,
+              builder: (context, _) {
+                return Positioned(
+                    left: 0,
+                    bottom: 0,
+                    width: deviceWidth,
+                    height: 100 * (_animation.value as double),
+                    child: Align(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _loadingStatus = true;
+                            _animationController.reset();
+                            _onLoad();
+                          });
+                        },
+                        child: const Text('次の20件を表示'),
+                      ),
+                    ));
+              }),
+        ),
+        Visibility(
+          visible: _isActiveUpperSide && _loadingStatus,
+          child: const Align(
+              alignment: Alignment.topCenter,
+              child: CircularProgressIndicator()),
+        ),
+        Visibility(
+          visible: !_isActiveUpperSide && _loadingStatus,
+          child: const Align(
+              alignment: Alignment.bottomCenter,
+              child: CircularProgressIndicator()),
+        ),
+      ],
+    );
   }
 
-  void initData() {
+  void _onInit() {
     for (var i = 0; i < 20; i++) {
       _items.insert(_items.length, 'original data ${_items.length}');
     }
@@ -75,23 +196,24 @@ class _MyListViewPageState extends State {
   void _onLoad() {
     Future<void>.delayed(const Duration(seconds: 2)).then((e) {
       print('_onLoad');
-      for (var i = 0; i < 20; i++) {
-        _items.insert(_items.length, 'loaded data ${_items.length}');
-      }
-      setState(() {});
-      _lock = false;
+      setState(() {
+        for (var i = 0; i < 20; i++) {
+          _items.insert(_items.length, 'loaded data ${_items.length}');
+        }
+        _loadingStatus = false;
+      });
     });
   }
 
-  Future _onRefresh() async {
+  void _onRefresh() {
     print('_onRefresh');
-    await Future<void>.delayed(const Duration(seconds: 2)).then((e) {
+    Future<void>.delayed(const Duration(seconds: 2)).then((e) {
       setState(() {
         _items.clear();
         for (var i = 0; i < 20; i++) {
           _items.insert(_items.length, 'refreshed data ${_items.length}');
         }
-        _lock = false;
+        _loadingStatus = false;
       });
     });
   }
